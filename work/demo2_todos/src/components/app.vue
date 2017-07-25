a4ac75d7f625c8ee70cd2100e3e4c94d3cad6b43
zxfjd3g <258147149@qq.com>
2017-07-25 11:48:16 +0800

    todo

---
+++ work/demo2_todos/src/components/app.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <demo-header :add="add"></demo-header>
+      <list :todos="todos" :delete-todo="deleteTodo"></list>
+      <demo-footer></demo-footer>
+    </div>
+  </div>
+</template>
+<script>
+  import header from './header.vue'
+  import list from './list.vue'
+  import footer from './footer.vue'
+
+  export default {
+
+    data () {
+      return {
+        todos: [
+          {title: '吃饭', complete: false},
+          {title: '睡觉', complete: true},
+          {title: 'test', complete: false},
+        ]
+      }
+    },
+
+    methods: {
+      add (todo) {
+        this.todos.unshift(todo)
+      },
+      deleteTodo (index) {
+        this.todos.splice(index, 1)
+      }
+    },
+
+    components: {
+      'demo-header': header,
+      list,
+      'demo-footer': footer
+    }
+  }
+</script>
+<style>
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+</style>
---
+++ work/demo2_todos/src/components/list.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul class="todo-main">
+    <item v-for="(todo, index) in todos" :key="index" :todo="todo"
+          :delete-todo="deleteTodo" :index="index"></item>
+  </ul>
+</template>
+
+<script>
+  import item from './item.vue'
+
+  export default {
+    props: {
+      todos: Array,
+      deleteTodo: Function
+    },
+
+    components: {
+      item
+    }
+  }
+</script>
+
+<style>
+  /*main*/
+  .todo-main {
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+  }
+
+  .todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+  }
+</style>
